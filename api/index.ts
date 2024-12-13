cf16a5c93ef9167306c7f69901ae938b1f07c713
David Hernández <hello@davidhernandez.info>
2024-12-13 11:49:18 +0100

    Adds CI to build images. Some cleaning of package.json and removed unused image.

--- api/index.ts
+++ api/index.ts
@@ -15,6 +15,7 @@ app.get('/', (req, res) => {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="og:description" content="User-Agent: ${userAgent}">
       <meta name="description" content="User-Agent: ${userAgent}">
     </head>
     <body>
